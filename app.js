9728017ee8db786932aeba2726edd0f160fb74fd
Minho Lee <minho.lee.93@hotmail.com>
2017-12-02 11:31:38 -0500

    deleting unnecessary code

--- app.js
+++ app.js
@@ -8,27 +8,33 @@ var cfenv = require('cfenv');
 
 
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req,res,next) {
+	// res.header('Access-Control-Allow-Origin', "x-requested-with");
+	// res.header("Access-Control-Allow-Headers", "*");
+	next();
+})
+
 app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-    res.render('main.ejs');
+	res.render('main.ejs');
 });
 
 app.get('/app', function(req, res) {
-    res.render('app.ejs');
+	res.render('app.ejs');
 });
 
 var appEnv = cfenv.getAppEnv();
 // console.log(appEnv);
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-    // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
+	// print a message when the server starts listening
+	console.log("server starting on " + appEnv.url);
 });
